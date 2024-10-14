28741c98e65a42336f2ecc8e5a1f8b37fe7ae3ec
semajame <jamesanquillano@gmail.com>
2024-10-14 15:37:43 +0800

    update

---
+++ mysql/hrdm1.sql
@@ -0,0 +1,18 @@
+INSERT INTO `hrdm` (subject_code, subject, units, preq, year) VALUES
+
+('NSTP1', 'National Service Training Prog. 1', 3, '-', 1),
+('Ethics', 'Ethics', 3, '-', 1),
+('PE1', 'Physical Education 1', 2, '-', 1),
+('Purcom', 'Purposive Communication', 3, '-', 1),
+('UTS', 'Understanding the Self', 3, '-', 1),
+('FIL 1', 'Retorika', 3, '-', 1),
+('ARTAPP', 'Art Appreciation', 3, '-', 1),
+('HIST', 'Readings in Philippine History', 3, '-', 1),
+('Law 1', 'Law on Obligations & Contracts', 3, '-', 1),
+('HRDM 1', 'Administrative & Office Management', 3, '-', 1),
+('Tax 1', 'Income Taxation', 3, '-', 1),
+('PE2', 'Physical Education 2', 2, 'PE1', 1),
+('NSTP 2', 'National Service Training & Prog. 2', 3, 'NSTP 1', 1),
+('FIL2', 'Panitikan ng Pilipinas', 3, 'FIL1', 1),
+('STS', 'Science, Technology & Society', 3, '-', 1),
+('CW', 'The Contemporary World', 3, '-', 1),
