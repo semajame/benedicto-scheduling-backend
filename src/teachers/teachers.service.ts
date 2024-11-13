3c0bd14a3087dc6a4752b8e709023886e8cfe327
semajame <jamesanquillano@gmail.com>
2024-11-13 15:59:59 +0800

    update api

--- src/teachers/teachers.service.ts
+++ src/teachers/teachers.service.ts
@@ -35,6 +35,7 @@ export class TeacherService {
         end: true,
         day: true,
         semester: true,
+        semester_id: true,
         school_year: true,
         recurrencePattern: true,
         background: true,
