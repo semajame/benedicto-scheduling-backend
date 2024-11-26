11b375bd9487cce105ff6d3b20f59311c10c02d8
semajame <jamesanquillano@gmail.com>
2024-11-26 14:12:03 +0800

    update

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -17,7 +17,7 @@ import { JwtStrategy } from './guards/jwt.strategy';
       imports: [ConfigModule],
       useFactory: async () => ({
         secret: process.env.JWT_SECRET,
-        signOptions: { expiresIn: '1m' }, // "10h", "7d"
+        signOptions: { expiresIn: '5s' }, // "10h", "7d"
       }),
       inject: [ConfigService],
     }),
